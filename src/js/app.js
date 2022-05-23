f0c8545d227713f564dd7e130377e595c8056416
Konstantin Beloglazov <ajoq@ya.ru>
2022-05-23 15:55:45 +0300

    dev 2

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,46 @@
-/* 
-    1. Сделать визуальную часть + добавить МИР, т.е. написать стили
-    2. Сделать функцию проверки валидности номера карты + добавление текста об успешной / неуспешной проверки
-    3. Функция выяснения принадлежности карты
-*/
-
 const form = document.getElementById('form');
+const cards = Array.from(document.querySelectorAll('.cards-item'));
 const formInput = document.getElementById('card-number');
+const tooltip = document.querySelector('.tooltip');
+const luhnSucces = document.querySelector('.luhn-succes');
+const luhnError = document.querySelector('.luhn-error');
+const noBankError = document.querySelector('.no-bank');
+
+formInput.addEventListener('input', (e) => {
+    clearMessages();
+    clearCardStyles();
+    checkBank(e.target.value);
+    if (e.target.value === '') return;
+    checkCardNumber(e.target.value);
+});
 
 form.addEventListener('submit', (e) => formSubmit(e));
 
 function formSubmit(e) {
     e.preventDefault();
-    
-    const inputText = formInput.value.trim();
+    clearMessages();
 
-    // console.log(checkLuhn(inputText));
-    checkLuhn(inputText);
+    let num = formInput.value.trim();
+
+    if (!checkCardNumber(num)) return;
+
+    if (checkLuhn(num)) {
+        luhnSucces.classList.add('display');
+    } else {
+        luhnError.classList.add('display');
+    }
+
+    if (!checkBank(num)) {
+        console.log('К сожалению, принадлежность карты не определена'); // Тут не корректно, поправить
+    }
 }
 
 function checkLuhn(value) {
     let ch = 0;
-    const num = String(value).replace(/\D/g, '');
+    const num = value;
     const isOdd = num.length % 2 !== 0;
 
-    if ('' === num) return false;
+    if (value.length < 12) return false;
 
     for (let i = 0; i < num.length; i++) {
         let n = parseInt(num[i], 10);
@@ -34,4 +51,76 @@ function checkLuhn(value) {
     return 0 === (ch % 10);
 }
 
-// console.log(checkLuhn(371449635398431));
+function checkCardNumber(value) { 
+
+    if (isNaN(value)) {
+        showNumCheckError();
+        return false;
+    } 
+
+    return value;
+}
+
+function showNumCheckError() {
+    tooltip.classList.add('display');
+}
+
+function clearMessages() {
+    luhnSucces.classList.remove('display');
+    luhnError.classList.remove('display');
+    tooltip.classList.remove('display');
+    noBankError.classList.remove('display');
+}
+
+function clearCardStyles() {
+    cards.forEach(item => {
+        item.classList.remove('cards-opacity');
+    })
+}
+
+function checkBank(value) {
+    //VISA
+    if (value.startsWith(4)) {
+        showBank('.visa');
+    }
+
+    //MasterCard
+    if (value.match(/^5[1-5]/) || value.substring(0, 4) >= 2221 && value.substring(0, 4) <= 2720) {
+        showBank('.master');
+    }
+
+    // // МИР
+    if (value.match(/^220[0-4]/)) {
+        showBank('.mir');
+    }
+
+    //American Express
+    if (value.match(/^3[47]/)) {
+        showBank('.amex');
+    }
+
+    //Discover
+    if (value.startsWith(6011)) {
+        showBank('.discover');
+    }
+
+    //JCB
+    if (value.startsWith(2131) || value.startsWith(1800) || value.startsWith(35)) {
+        showBank('.jcb');
+    }
+
+    // Diners Club
+    if (value.match(/^30[0-5]/) || value.startsWith(36) || value.startsWith(38)) {
+        showBank('.diners_club');
+    }
+
+    return;
+}
+
+function showBank(bankName) {
+    cards.forEach(item => {
+        item.classList.add('cards-opacity');
+    });
+
+    document.querySelector(bankName).closest('.cards-opacity').classList.remove('cards-opacity'); 
+}
