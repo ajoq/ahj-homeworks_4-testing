18832523460803f1f0e5be38eee6f22029a97e8d
Konstantin Beloglazov <ajoq@ya.ru>
2022-05-31 15:06:48 +0300

    settings upd4

--- src/js/e2e/e2e.test.js
+++ src/js/e2e/e2e.test.js
@@ -1,22 +1,36 @@
 import puppeteer from 'puppeteer';
+import { fork } from 'child_process';
 
 jest.setTimeout(30000);
+
 describe('Card number form', () => {
   let browser = null;
   let page = null;
   const baseUrl = 'http://localhost:8888';
 
   beforeAll(async () => {
+    server = fork(`${__dirname}/e2e.server.js`);
+
+    await new Promise((resolve, reject) => {
+        server.on('error', reject);
+        server.on('message', (message) => {
+          if (message === 'ok') {
+            resolve();
+          }
+        });
+      })
+
     browser = await puppeteer.launch({
-      headless: true,
-      slowMo: 100,
-      devtools: false,
+    //   headless: true,
+    //   slowMo: 100,
+    //   devtools: false,
     });
     page = await browser.newPage();
   });
 
   afterAll(async () => {
     await browser.close();
+    server.kill();
   });
 
   test('Valid card number', async () => {
